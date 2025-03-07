56476575069f9acea83757b7d4bdc59561d4e6fe
Samuel Breu <samuel@breu.cloud>
2025-03-07 17:25:23 +0100

    code cleanup of Mailxpert provider

--- src/Provider/Mailxpert.php
+++ src/Provider/Mailxpert.php
@@ -10,7 +10,6 @@ use League\OAuth2\Client\Provider\GenericResourceOwner;
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 use League\OAuth2\Client\Token\AccessToken;
 use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
-use Mailxpert\OAuth2\Client\Exception\ResourceOwnerException;
 use Psr\Http\Message\ResponseInterface;
 
 class Mailxpert extends AbstractProvider
@@ -19,39 +18,21 @@ class Mailxpert extends AbstractProvider
 
     protected $baseHost = 'mailxpert.ch';
 
-    /**
-     * Get authorization url to begin OAuth 2.0 'Authorization Code' grant.
-     */
     public function getBaseAuthorizationUrl(): string
     {
         return 'https://app.' . $this->baseHost . '/auth/v3/authorize';
     }
 
-    /**
-     * Get access token url to retrieve token.
-     */
     public function getBaseAccessTokenUrl(array $params): string
     {
         return 'https://app.' . $this->baseHost . '/auth/v3/token';
     }
 
-    /**
-     * We do currently not support an owner resource.
-     *
-     * @param AccessToken $token
-     *
-     * @return string|void
-     */
-    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    public function getResourceOwnerDetailsUrl(AccessToken $token): string
     {
         return 'https://api.' . $this->baseHost . '/v3/me';
     }
 
-    /**
-     * Get the default scopes uses by this provider.
-     *
-     * @return array
-     */
     protected function getDefaultScopes(): array
     {
         return [
@@ -60,11 +41,7 @@ class Mailxpert extends AbstractProvider
         ];
     }
 
-    /**
-     * @param array|string $data
-     *
-     * @throws IdentityProviderException
-     */
+    /** @inheritDoc */
     protected function checkResponse(ResponseInterface $response, $data): void
     {
         $statusCode = $response->getStatusCode();
@@ -74,12 +51,7 @@ class Mailxpert extends AbstractProvider
         }
     }
 
-    /**
-     * @throws ResourceOwnerException
-     *
-     * @return ResourceOwnerInterface|void
-     */
-    protected function createResourceOwner(array $response, AccessToken $token)
+    protected function createResourceOwner(array $response, AccessToken $token): ResourceOwnerInterface
     {
         return new GenericResourceOwner($response, 'uid');
     }
